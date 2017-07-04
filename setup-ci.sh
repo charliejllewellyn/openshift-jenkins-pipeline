d1c66cfc094052dc4aa65b9f12a8fa790cacc7a2
Cloud User <cloud-user@bastion-host.localdomain>
2017-07-04 10:14:11 -0400

    Add vars needed for openstack

--- setup-ci.sh
+++ setup-ci.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo 'Please enter the password for the openstack CI user (openshift@ukcloud.com)'
+read password
+
 projects=(build-openshift build-openshift-pre-prod)
 
 for project in $projects: do
@@ -10,3 +13,6 @@ oc project build-openshift
 oc new-build https://github.com/charliejllewellyn/openshift-jenkins-pipeline.git --context-dir=docker/openstack-jenkins-slave/
 oc policy add-role-to-user edit system:serviceaccount:build-openshift:jenkins -n build-openshift-pre-prod
 oc new-build https://github.com/charliejllewellyn/openshift-jenkins-pipeline.git --context-dir=jenkins-pipelines/openstack/
+oc create -f openstack_params.yaml
+
+oc create secret generic openstack --from-literal=username=openshift@ukcloud.com --from-literal=password=$password
