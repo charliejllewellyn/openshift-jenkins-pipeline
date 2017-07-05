cb6e60873c7b944801cc6d6a85daef83a118cb81
Cloud User <cloud-user@bastion-host.localdomain>
2017-07-05 06:10:19 -0400

    Jenkins shizzle

--- setup-ci.sh
+++ setup-ci.sh
@@ -3,23 +3,34 @@
 echo 'Please enter the password for the openstack CI user (openshift@ukcloud.com)'
 read password
 
-projects=(build-openshift build-openshift-pre-prod)
+function setup_projects() {
+    projects=(build-openshift build-openshift-pre-prod)
+    for project in $projects: do
+        oc new-project $project
+    done
+    oc policy add-role-to-user edit system:serviceaccount:build-openshift:jenkins -n build-openshift-pre-prod
+}
 
-for project in $projects: do
-    oc new-project $project
-done
+function setup_openstack_jenkins() {
+    oc project build-openshift
+    oc new-build https://github.com/charliejllewellyn/openshift-jenkins-pipeline.git --context-dir=docker/openstack-jenkins-slave/
+    oc new-build https://github.com/charliejllewellyn/openshift-jenkins-pipeline.git --context-dir=jenkins-pipelines/openstack/
+}
 
-oc project build-openshift
-oc new-build https://github.com/charliejllewellyn/openshift-jenkins-pipeline.git --context-dir=docker/openstack-jenkins-slave/
-oc policy add-role-to-user edit system:serviceaccount:build-openshift:jenkins -n build-openshift-pre-prod
-oc new-build https://github.com/charliejllewellyn/openshift-jenkins-pipeline.git --context-dir=jenkins-pipelines/openstack/
-oc create -f openstack_params.yaml
+function setup_openshift_pipeline() {
+    oc create -f openstack_params.yaml
+    oc create secret generic openstack --from-literal=username=openshift@ukcloud.com --from-literal=password=$password
+    oc create secret generic rhelsubscriptions --from-literal=rhel_org=6468465 --from-literal=rhel_activation_key=openshift
+    oc new-build https://github.com/charliejllewellyn/openshift-jenkins-pipeline.git --context-dir=jenkins-pipelines/openshift/ --name=openshift-build
+}
 
-oc create secret generic openstack --from-literal=username=openshift@ukcloud.com --from-literal=password=$password
-oc create secret generic rhelsubscriptions --from-literal=rhel_org=6468465 --from-literal=rhel_activation_key=openshift
+function configure_openshift_githooks() {
+    # TODO: Automate webhook creation and updates via the github API
+    gitHook=$(oc describe bc hopeful | grep -A1 'Webhook GitHub' | grep URL | awk '{print $NF}')
+    echo "Add a github webhook for the following URL to trigger automated builds of the Jenkins pipeline, $gitHook"
+}
 
-oc new-build https://github.com/charliejllewellyn/openshift-jenkins-pipeline.git --context-dir=jenkins-pipelines/openshift/ --name=openshift-build
-
-# TODO: Automate webhook creation and updates via the github API
-gitHook=$(oc describe bc hopeful | grep -A1 'Webhook GitHub' | grep URL | awk '{print $NF}')
-echo "Add a github webhook for the following URL to trigger automated builds of the Jenkins pipeline, $gitHook"
+setup_projects
+setup_openstack_jenkins
+setup_openshift_pipeline
+configure_openshift_githooks
