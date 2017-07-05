f1a104fabdecc3efbcc8494a9d4d2d5f4122f345
Cloud User <cloud-user@bastion-host.localdomain>
2017-07-05 06:37:16 -0400

    Correct array

--- setup-ci.sh
+++ setup-ci.sh
@@ -5,20 +5,31 @@ read password
 
 function setup_projects() {
     projects=(build-openshift build-openshift-pre-prod)
-    for project in $projects: do
+    for project in ${projects[@]}; do
         oc new-project $project
     done
     oc policy add-role-to-user edit system:serviceaccount:build-openshift:jenkins -n build-openshift-pre-prod
 }
 
 function setup_openstack_jenkins() {
+
+    NAME='openstack-jenkins-slave'
+    SOURCE_REPOSITORY_URL='https://github.com/charliejllewellyn/openshift-jenkins-pipeline.git'
+    SOURCE_REPOSITORY_REF='master'
+    CONTEXT_DIR='docker/openstack-jenkins-slave/'
+    PIPELINE_CONTEXT_DIR='jenkins-pipelines/openstack/'
+
     oc project build-openshift
-    oc new-build https://github.com/charliejllewellyn/openshift-jenkins-pipeline.git --context-dir=docker/openstack-jenkins-slave/
-    oc new-build https://github.com/charliejllewellyn/openshift-jenkins-pipeline.git --context-dir=jenkins-pipelines/openstack/
+    oc new-app -f openshift-yaml/template-openstackclient-jenkins-slave.yaml \
+        -p NAME=$NAME \
+        -p SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL \
+        -p SOURCE_REPOSITORY_REF=$SOURCE_REPOSITORY_REF \
+        -p CONTEXT_DIR=$CONTEXT_DIR \
+        -p PIPELINE_CONTEXT_DIR=$PIPELINE_CONTEXT_DIR
 }
 
 function setup_openshift_pipeline() {
-    oc create -f openstack_params.yaml
+    oc create -f openshift-yaml/openstack_params.yaml
     oc create secret generic openstack --from-literal=username=openshift@ukcloud.com --from-literal=password=$password
     oc create secret generic rhelsubscriptions --from-literal=rhel_org=6468465 --from-literal=rhel_activation_key=openshift
     oc new-build https://github.com/charliejllewellyn/openshift-jenkins-pipeline.git --context-dir=jenkins-pipelines/openshift/ --name=openshift-build
